0f054c252850aba761d4946d6b483f59fca6bb12
Jeff Brubaker <brubaker.jeff@gmail.com>
2014-12-09 21:21:39 -0500

    initial

--- platforms/ios/www/scripts/controllers/main.js
+++ platforms/ios/www/scripts/controllers/main.js
@@ -10,7 +10,7 @@
 angular.module('perambulatorApp')
   .controller('MainCtrl', function ($scope) {
  
-
+      var gWatchID1=null;
     	$scope.Lat1=0;//po sition.coords.latitude;
     	$scope.Long1=0;//position.coords.longitude;
     	$scope.Heading1=0;//position.coords.heading;
@@ -20,61 +20,26 @@ angular.module('perambulatorApp')
     	$scope.a="";
     	$scope.b="";
     	$scope.c="";
+		  $scope.mu="";
+		  $scope.lambda="";
+      var onSuccessH = function(position) {
+        //debugger;
+      	$scope.Heading1=position.coords.heading;
+        $scope.Lat1=position.coords.latitude;
+        $scope.Long1=position.coords.longitude;
+        $scope.calculate();
+        $scope.$apply();
+  	};
 
-        var onSuccessH = function(heading) {
-    	$scope.Heading1=heading.magneticHeading;
-  /* alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
+    var onSuccessH2 = function(position) {
+      $scope.Lat2=position.coords.latitude;
+      $scope.Long2=position.coords.longitude;
+    	$scope.Heading2=position.coords.heading;
+      $scope.calculate();
+      $scope.$apply();
+
 	};
 
-    var onSuccessH2 = function(heading) {
-
-    	$scope.Heading2=heading.magneticHeading;
-  /* alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
-	};
-
-
-
-
-    var onSuccess = function(position) {
-    	$scope.Lat1=position.coords.latitude;
-    	$scope.Long1=position.coords.longitude;
-//    	$scope.Heading1=position.coords.heading;
-  /* alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
-	};
-    var onSuccess2 = function(position) {
-    	$scope.Lat2=position.coords.latitude;
-    	$scope.Long2=position.coords.longitude;
-  //  	$scope.Heading2=position.coords.heading;
-  /* alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');*/
-	};
     
     Math.asind = function(x) {
        return Math.asin(x)*180/Math.PI;
@@ -89,6 +54,12 @@ angular.module('perambulatorApp')
     };
 
     $scope.calculate = function() {
+
+      if($scope.Heading2=="0"||$scope.Heading1=="0")
+      {
+        return;
+      }
+
       var delta_latitude=Math.abs($scope.Lat1-$scope.Lat2);
     	var delta_longitude=Math.abs($scope.Long1-$scope.Long2);
 
@@ -98,40 +69,80 @@ angular.module('perambulatorApp')
       var LatScaleFactor=Math.cosd(($scope.Lat1+$scope.Lat2)/2);
       var KMperLonDegree=EarthCircEquator/360*LatScaleFactor;
       var KMperLatDegree=EarthCircPoles/360;
-      debugger;
+      //debugger;
       var ss1=delta_longitude*KMperLonDegree;
       var ss2=delta_latitude*KMperLatDegree;
 
-      var a=Math.sqrt(Math.pow(ss1,2)+Math.pow(ss2,2));
-      var aa=Math.abs($scope.Heading1-$scope.Heading2);
-    	var theta=Math.asind(ss2/a);
-    	var phi=180-90-theta;
-      var cc=360-phi-$scope.Heading1;
-
-      var bb=180-aa-cc;
-      var b=Math.sind(bb)*a/Math.sind(aa);
-      var c=Math.sind(cc)*a/Math.sind(aa);
-    	
-    	$scope.a=a;
-    	$scope.b=b;
-    	$scope.c=c;
-
+     
+		
+  		var x1 = Math.sind($scope.Heading1);
+  		var x2 = Math.sind($scope.Heading2);
+  		var y1= Math.cosd($scope.Heading1);
+  		var y2 = Math.cosd($scope.Heading2);
+  		
+  		var numlambda = -y2*ss1+x2*ss2;
+  		var nummu = -y1*ss1+x1*ss2;
+  		var den = -x1*y2+x2*y1;
+  		$scope.mu = nummu/den;
+  		$scope.lambda = numlambda/den;
     }
     $scope.getDimension1 = function() {
-		navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-
-		navigator.compass.getCurrentHeading(onSuccessH, onError);
-		$scope.calculate();
+	//	  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		  if(gWatchID1==null) {
+        startWatch1();
+		  }else {
+        stopWatch1();
+        stopWatch2();
+      }
+      $scope.calculate();
     }
 
-        $scope.getDimension2 = function() {
-		navigator.geolocation.getCurrentPosition(onSuccess2, onError);
+    $scope.getDimension2 = function() {
+		//  navigator.geolocation.getCurrentPosition(onSuccess2, onError);
+		//  navigator.compass.getCurrentHeading(onSuccessH2, onError);
+		  if(gWatchID1==null) {
+        startWatch2();
+      }else {
+        stopWatch1();
+        stopWatch2();
+      }
+      $scope.calculate();
 
-		navigator.compass.getCurrentHeading(onSuccessH2, onError);
+    }
 
-		$scope.calculate();
+    function startWatch1() {
 
+        // Update compass every 3 seconds
+        var options = { frequency: 500 };
+
+        if (gWatchID1==null)
+            gWatchID1 = navigator.geolocation.watchPosition(onSuccessH, onError, options);
+    }
+
+
+    function stopWatch1  () {
+        if (gWatchID1!=null) {
+            navigator.geolocation.clearWatch(gWatchID1);
+            gWatchID1= null;
+        }
+    }
+
+    function startWatch2() {
+
+        // Update compass every 3 seconds
+        var options = { frequency: 500 };
+
+        if (gWatchID1==null)
+            gWatchID1 = navigator.geolocation.watchPosition(onSuccessH2, onError, options);
+    }
+
+
+    function stopWatch2  () {
+        if (gWatchID1!=null) {
+            navigator.geolocation.clearWatch(gWatchID1);
+            gWatchID1= null;
+        }
     }
 
 // onError Callback receives a PositionError object
